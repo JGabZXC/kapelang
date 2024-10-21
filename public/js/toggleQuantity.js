381cc66a72b9d06b1b97a5c521d4b79065250e41
JGabZXC <jlopezgab25@gmail.com>
2024-10-21 21:20:37 +0800

    Fixed toggleQuantity

--- public/js/toggleQuantity.js
+++ public/js/toggleQuantity.js
@@ -1,11 +1,20 @@
 "use strict";
 
-function toggleQuantity(id, totalId) {
+function toggleQuantity(id, totalId, totalPriceId, price) {
   const quantityInput = document.querySelector(`#${id}`);
   const totalDisplay = document.querySelector(`#${totalId}`);
+  const totalPriceDisplay = document.querySelector(`#${totalPriceId}`);
+
   if (quantityInput.style.display === "none") {
     quantityInput.style.display = "block";
-    // totalDisplay.style.display = "block";
+
+    const quantity = 1;
+    const totalPrice = quantity * Number(price);
+    quantityInput.value = quantity;
+
+    totalDisplay.style.display = quantity > 0 ? "inline" : "none";
+    totalDisplay.textContent = `Total: ₱${totalPrice}`;
+    totalPriceDisplay.value = `${totalPrice}`;
     quantityInput.focus();
   } else {
     quantityInput.style.display = "none";
